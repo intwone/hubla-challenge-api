8b780087126e259485962cb05fd16b448217fc1c
intwone <cassiointw1993@gmail.com>
2022-10-05 11:31:34 -0300

    add mock to simulate database operations

---
+++ __tests__/mocks/memory-transaction-repository.ts
@@ -0,0 +1,42 @@
+import { TransactionProtocol } from '@src/modules/transactions/protocols';
+import { TransactionRepositoryProtocol } from '@src/modules/transactions/protocols/transaction-repository-protocol';
+import { randomUUID } from 'crypto';
+
+export default class MemoryTransactionRepository
+  implements TransactionRepositoryProtocol
+{
+  public transactions: TransactionProtocol[];
+
+  constructor() {
+    this.transactions = [];
+  }
+
+  async list(): Promise<TransactionProtocol[] | [] | Error> {
+    return this.transactions;
+  }
+
+  async insert(
+    transaction: TransactionProtocol,
+  ): Promise<TransactionProtocol | null | Error> {
+    const newTransaction = {
+      id: randomUUID(),
+      type: Number(transaction.type),
+      date: transaction.date,
+      productName: transaction.productName,
+      value: transaction.value,
+      seller: transaction.seller,
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+
+    this.transactions.push(newTransaction);
+
+    return {
+      type: newTransaction.type,
+      date: newTransaction.date,
+      productName: newTransaction.productName,
+      value: newTransaction.value,
+      seller: newTransaction.seller,
+    };
+  }
+}
