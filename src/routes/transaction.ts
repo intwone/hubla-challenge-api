65cf3f49faae4c54ca220ecf146fc944d89de844
intwone <cassiointw1993@gmail.com>
2022-10-05 00:33:11 -0300

    add router to manage find transaction requests

--- src/routes/transaction.ts
+++ src/routes/transaction.ts
@@ -1,16 +1,21 @@
 import { multerConfig } from '@src/config';
 import { InsertTransactionController } from '@src/modules/transactions/controllers/insert-transaction-controller';
+import { ListTransactionsController } from '@src/modules/transactions/controllers/list-transactions-controller';
 import express from 'express';
 import multer from 'multer';
 
 const router = express.Router();
 const upload = multer(multerConfig);
+
 const insertTransactionController = new InsertTransactionController();
+const listTransactionsController = new ListTransactionsController();
 
 router.post(
   '/',
   upload.single('transactions'),
   insertTransactionController.handle,
 );
+
+router.get('/', listTransactionsController.handle);
 
 export default router;
